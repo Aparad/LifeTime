e0305f255a8f2022cf3ca26de94de2436d6643b6
Aparad <radoslawmatuszak1@gmail.com>
2017-08-12 12:10:33 +0200

    Add goal funcionality
    Working just fine

--- LifeTime Android/LifeTime Android/MainActivity.cs
+++ LifeTime Android/LifeTime Android/MainActivity.cs
@@ -6,59 +6,68 @@ using Android.Views;
 using Newtonsoft.Json;
 using System.Runtime.Serialization;
 using System.Collections.Generic;
+using Android.Content;
 
 namespace LifeTime_Android
 {
     [Activity(Label = "LifeTime", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        GoalMenu goalMenu = new GoalMenu();
+        Context _context;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
+            _context = this;
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            //Example data
-            GoalMenu goalMenu = new GoalMenu();
+            InsertTestGoals();
+            SetBindings();
+        }
+
+        public void InsertTestGoals()
+        {
+            //Adding sample goals and  data
+            ViewGroup goalsLayout = (LinearLayout)FindViewById(Resource.Id.goalsLayout);
+            var GoalActivityIntent = new Android.Content.Intent(this, typeof(GoalActivity));
+
             Model.Goal goal1 = new Model.Goal("Practice guitar");
             Model.DailyActivity act1 = new Model.DailyActivity("Arpeggios", "Shred some arpeggios!", 1, true);
             goal1.GoalActivities.Add(act1);
-            GoalMenu.AddGoal(goal1);
+            goalMenu.AddGoal(goal1, GoalActivityIntent, _context, goalsLayout);
 
             Model.Goal goal2 = new Model.Goal("Study for exams next week");
             Model.DailyActivity act2 = new Model.DailyActivity("Explosions", "BOOMbombombombombom", 1, true);
             Model.DailyActivity act3 = new Model.DailyActivity("Chemistry n' shit", "DO some magic.", 1, false);
             goal2.GoalActivities.Add(act2);
             goal2.GoalActivities.Add(act3);
-            GoalMenu.AddGoal(goal2);
-            GoalMenu.AddGoal(new Model.Goal("Relax", status: false));
+            goalMenu.AddGoal(goal2, GoalActivityIntent, _context, goalsLayout);
+            goalMenu.AddGoal(new Model.Goal("Relax", status: false), GoalActivityIntent, _context, goalsLayout);
+        }
 
-            ViewGroup goalsLayout = (LinearLayout)FindViewById(Resource.Id.goalsLayout);
-            var intent = new Android.Content.Intent(this, typeof(GoalActivity));
-            foreach (var goal in GoalMenu.MyGoals)
-            {
-                GoalMenu.PopulateGoalMenuList(goal, intent, this, goalsLayout);
-            }
-
+        public void SetBindings()
+        {
             Button addButton = (Button)FindViewById(Resource.Id.addButtonOutside);
             addButton.Click += delegate
             {
                 var addGoalIntent = new Android.Content.Intent(this, typeof(GoalAddActivity));
-                addGoalIntent.PutExtra("GoalsList", JsonConvert.SerializeObject(GoalMenu.MyGoals));
-                StartActivityForResult(addGoalIntent, 0);
+                addGoalIntent.PutExtra("GoalsList", JsonConvert.SerializeObject(goalMenu.MyGoals));
+                StartActivityForResult(addGoalIntent, 1);
             };
-        }
+        } 
 
-        public void onActivityResult(int requestCode, Result resultCode, Android.Content.Intent data)
+        protected override void OnActivityResult(int requestCode, Result resultCode, Android.Content.Intent data)
         {
             base.OnActivityResult(requestCode, resultCode, data);
             if (resultCode == Result.Ok)
             {
+                ViewGroup goalsLayout = (LinearLayout)FindViewById(Resource.Id.goalsLayout);
+                var GoalActivityIntent = new Android.Content.Intent(this, typeof(GoalActivity));
                 var GoalPassedBack = new Model.Goal();
-                GoalPassedBack.GoalName = data.GetStringExtra("GoalName");
-                GoalPassedBack.GoalDescription = data.GetStringExtra("GoalDescription");
-                GoalMenu.AddGoal(GoalPassedBack);
+                GoalPassedBack.GoalName = data.GetStringExtra("goalName");
+                GoalPassedBack.GoalDescription = data.GetStringExtra("goalDescription");
+                goalMenu.AddGoal(GoalPassedBack, GoalActivityIntent, _context, goalsLayout);
             }
         }
     }
--- LifeTime Android/LifeTime Android/ViewModel/GoalMenu.cs
+++ LifeTime Android/LifeTime Android/ViewModel/GoalMenu.cs
@@ -12,22 +12,23 @@ namespace LifeTime_Android.ViewModel
 {
     class GoalMenu
     {
-        static public List<Goal> MyGoals = new List<Goal>();
+        public List<Goal> MyGoals = new List<Goal>();
 
         //public void AddGoalButtonClick(Object sender, EventArgs e, Context context)
         //{
         //    var intent = new Android.Content.Intent(context, typeof(GoalAddActivity));
         //    context.StartActivity(intent);
         //}
-        static public void AddGoal(Goal goal)
+        public void AddGoal(Goal goal, Intent intent, Context context, ViewGroup Layout)
         {
             MyGoals.Add(goal);
+            PopulateGoalMenuList(goal, intent, context, Layout);
         }
-        static public void DeleteGoal(Goal goal)
+        public void DeleteGoal(Goal goal)
         {
             MyGoals.Remove(goal);
         }
-        static public void PopulateGoalMenuList(Goal goal, Intent intent, Context context, ViewGroup Layout) //Add goal button to GoalMenu
+        public void PopulateGoalMenuList(Goal goal, Intent intent, Context context, ViewGroup Layout) //Add goal button to GoalMenu
         {
             Button goalButton = new Button(context);
             goalButton.Text = goal.GoalName;
